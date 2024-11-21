e0a392dafa7a3b74fd980cc14e1680dd727c70dd
reymarkarididon <naelgagwen@gmail.com>
2024-11-20 23:21:10 -0800

    reymark.php

---
+++ reymark.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My First Ubuntu Server PHP Deployment</title>
+</head>
+<body>
+
+    <h2>Enter Your Information</h2>
+    <form method="post" action="">
+        <label for="first_name">First Name:</label>
+        <input type="text" id="first_name" name="first_name" required><br><br>
+        
+        <label for="last_name">Last Name:</label>
+        <input type="text" id="last_name" name="last_name" required><br><br>
+        
+        <label for="age">Age:</label>
+        <input type="number" id="age" name="age" required><br><br>
+        
+        <label for="address">Address:</label>
+        <textarea id="address" name="address" required></textarea><br><br>
+        
+        <label for="Course and section">Course and section:</label>
+        <input type="text" id="Course and section" name="Course and section" required><br><br>
+        <input type="submit" value="Submit">
+    </form>
+    
+</body>
+
+</html>
